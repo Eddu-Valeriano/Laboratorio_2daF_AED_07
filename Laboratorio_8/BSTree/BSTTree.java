eaa060a3707ad3c08f7bb5550caddc29849a6bcd
Eddu <eddu.valeriano@ucsm.edu.pe>
2025-05-24 21:53:46 -0500

    Clase BSTTree: aqui se cambia el acceso de los atributos como la clase nodo de la clase bstree como protegido para que pueda ser accedido a atraves de la clase AVLTree

---
+++ Laboratorio_8/BSTree/BSTTree.java
@@ -0,0 +1,17 @@
+package BSTree;
+
+
+public class BSTTree {
+    protected class Nodo {
+        int valor;
+        Nodo izquierdo, derecho;
+
+        Nodo(int valor) {
+            this.valor = valor;
+        }
+    }
+
+    protected Nodo raiz;
+
+    // Métodos del BST...
+}
